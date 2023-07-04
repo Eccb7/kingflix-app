18ea899926b03e2009259dccf0672e933c26bdf2
geekelo <efuelight12@gmail.com>
2023-07-04 13:22:20 +0100

    Add footer component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import printMe from './print.js';
 import './stylesheets/header.css';
+import './stylesheets/footer.css';
 import './media-library/logo.png';
 import Header from './header.js';
 import Footer from './footer.js';
@@ -8,4 +9,5 @@ import Footer from './footer.js';
 
 const headerSection = new Header();
 const footerSection = new Footer();
+
 export { headerSection, footerSection };
