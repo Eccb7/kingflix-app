1de37835ca62a91775eba8bb3e06f860631cba7b
Kingsley Igbor <chidoziek2@gmail.com>
2023-07-05 14:15:09 +0100

    created a homepage and rendered a list of movies

---
+++ src/getShows.js
@@ -0,0 +1,47 @@
+const getShows = async () => {
+    const homeContainer = document.querySelector(".homepage");
+    try {
+        const response = await fetch("https://api.tvmaze.com/shows/1/episodes");
+
+        if (!response.ok) {
+            throw new Error(`Failed to fetch scores: ${response.status}`);
+        }
+
+        const json = await response.json();
+        console.log(json);
+
+        json.forEach((movie) => {
+            const body = document.createElement("div");
+            body.className = "movies";
+            const images = document.createElement("div");
+            images.className = "test-img";
+            images.innerHTML = `<img src=${movie.image.medium} alt="" class="my-img">`;
+
+            const loveImage = document.createElement("img");
+            loveImage.src = "./media-library/love.png";
+            loveImage.className = "love";
+
+            const likes = document.createElement("div");
+            likes.className = "space";
+            likes.innerHTML = `<p>${movie.name}</p>`;
+            likes.innerHTML += ` <p>5 likes</p>`;
+            likes.appendChild(loveImage);
+
+            const button1 = document.createElement("button");
+            button1.className = "btn";
+            button1.innerText = "Comments";
+            const button2 = document.createElement("button");
+            button2.className = "btn";
+            button2.innerText = "Reservations";
+
+            body.appendChild(images);
+            body.appendChild(likes);
+            body.appendChild(button1);
+            body.appendChild(button2);
+            homeContainer.appendChild(body);
+        });
+    } catch (error) {
+        throw new Error("Unable to post");
+    }
+};
+export default getShows;
