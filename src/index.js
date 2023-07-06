79944499f11e3fc1ba10e65e1acd65fd256d4d3a
Briton Otieno <ojwangbriton@gmail.com>
2023-07-07 02:02:09 +0300

    Update index.js file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import './stylesheets/header.css';
 import './stylesheets/footer.css';
 import './stylesheets/homepage.css';
+import './stylesheets/comments.css';
 import './media-library/logo.png';
 import Header from './header.js';
 import Footer from './footer.js';
 import Home from './home.js';
+// import createModal from './modal';
 
 const headerSection = new Header();
 const homeSection = new Home();
