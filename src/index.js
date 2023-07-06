1b8ef92759b0f897cf5bdd3a1ab794cb5b0c9205
Kingsley Igbor <chidoziek2@gmail.com>
2023-07-06 11:00:05 +0100

    commiting the rest files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import "./stylesheets/header.css";
-import "./stylesheets/footer.css";
-import "./stylesheets/homepage.css";
-import "./media-library/logo.png";
-import Header from "./header.js";
-import Footer from "./footer.js";
-import Home from "./home";
+import './stylesheets/header.css';
+import './stylesheets/footer.css';
+import './stylesheets/homepage.css';
+import './media-library/logo.png';
+import Header from './header.js';
+import Footer from './footer.js';
+import Home from './home.js';
 
 const headerSection = new Header();
 const homeSection = new Home();
